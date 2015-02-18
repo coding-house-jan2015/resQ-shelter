b58fee6d6b0882d495091a72fff5c91bb0d618e9
Chyld Medford <chyld.medford@gmail.com>
2015-02-17 23:00:04 -0800

    more animals

--- server/routes/animals/update.js
+++ server/routes/animals/update.js
@@ -5,7 +5,7 @@ var Animal = require('../../models/animal');
 module.exports = {
   handler: function(request, reply) {
     Animal.findByIdAndUpdate(request.params.animalId, request.payload, function() {
-      reply.redirect('/animals/' + request.params.animalId)
+      reply.redirect('/animals/' + request.params.animalId);
     });
   }
 };
