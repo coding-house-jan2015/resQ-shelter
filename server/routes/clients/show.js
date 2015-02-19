aa89f1c0b1706abe07db52460bff99913fea0df0
Chyld Medford <chyld.medford@gmail.com>
2015-02-18 16:23:19 -0800

    fin

--- server/routes/clients/show.js
+++ server/routes/clients/show.js
@@ -6,7 +6,6 @@ var Client = require('../../models/client');
 module.exports = {
   handler: function(request, reply) {
     Client.findOne({_id:request.params.clientId}).populate('pets').exec(function(err, client) {
-      console.log('******client*******', client);
       reply.view('templates/clients/show', {path:'/clients', active:active, client:client});
     });
   }
